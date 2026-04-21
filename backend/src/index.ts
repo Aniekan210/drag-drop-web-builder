eca3d827714d7dc9a18ba3ee740625ed1da561ac
Aniekan210 <ani8dar@gmail.com>
2026-04-21 16:17:36 -0300

    added boiler plate express with ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,28 @@
+import "dotenv/config";
 import express from "express";
+import { neon } from "@neondatabase/serverless";
 
 const app = express();
+const DATABASE_CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING;
 
-app.get("/", (req, res) => {
-  res.send("Hello TypeScript Express");
+if (!DATABASE_CONNECTION_STRING) {
+  throw new Error("DATABASE_CONNECTION_STRING is not defined");
+}
+export const SQL = neon(DATABASE_CONNECTION_STRING);
+
+// BOILERPLATE REQUEST HANDLER
+app.get("/", async (req, res) => {
+  try {
+    const result = await SQL`SELECT version()`;
+    const version = result[0]?.version;
+
+    res.send(`Hello TypeScript Express\nDB: ${version}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Database error");
+  }
 });
+
 
 app.listen(8080, () => {
   console.log("Server running on port 8080");
